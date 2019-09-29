ad83111bfb89c29a3c07f12627be3f1c91b270b6
Avik Kadakia <avik.kadakia@stonybrook.edu>
2019-09-29 08:14:55 -0400

    Finished jsTPS_Transaction and started working on jsTPS

---
+++ jsTPS/src/jsTPS.js
@@ -0,0 +1,59 @@
+'strict mode'
+
+import jsTPS_Transaction from jsTPS
+
+/**
+ * jsTPS.js
+ * 
+ * This class is used for managing an abstract transaction processing
+ * system for the purpose of managing an undo/redo system for an
+ * application. Note that one must specify all work done via custom
+ * transactions.
+ * 
+ * @author Avik Kadakia
+ * @version 0.0
+ */
+
+ class jsTPS{
+
+    constructor()
+    {
+        // THE TRANSACTION STACK
+        this.transactions = [];
+
+        // KEEPS TRACK OF WHERE WE ARE IN THE STACK, THUS AFFECTING WHAT
+        // TRANSACTION MAY BE DONE OR UNDONE AT ANY GIVEN TIME
+        this.mostRecentTransaction = -1;
+
+        // THESE VARIABLES CAN BE TURNED ON AND OFF TO SIGNAL THAT
+        // DO AND UNDO OPERATIONS ARE BEING PERFORMED
+        this.performingDo = false;
+        this.performingUndo = false;
+    }
+
+    /**
+     * Tests to see if the undo operation is currently being
+     * performed. If it is, true is returned, if not, false.
+     * 
+     * @return true if the undo operation is currently in the
+     * process of executing, false otherwise.
+     */
+    isPerformingDo()
+    {
+        return this.performingDo;
+    }
+
+    /**
+     * This function adds the transaction argument to the top of
+     * the transaction processing system stack and then executes it. Note that it does
+     * When this method has completed transaction will be at the top 
+     * of the stack, it will have been completed, and the counter have
+     * been moved accordingly.
+     * 
+     * @param transaction The custom transaction to be added to
+     * the transaction processing system stack and executed.
+     */
+    addTransaction()
+     
+    
+ }
---
+++ jsTPS/src/jsTPS_Transaction.js
@@ -0,0 +1,35 @@
+'strict mode'
+
+/**
+ * jsTPS_Transaction.java
+ * 
+ * This interface provides the structure that all transactions to be
+ * managed by jsTPS must follow. Each transaction must have defined
+ * behavior for do and undo. Note that when defining custom transaction
+ * classes one needs to make sure the constructor is given the objects
+ * it will need to manipulate at the time the do and undo methods
+ * are called.
+ * 
+ * @author Avik Kadakia
+ * @version 1.0
+ */
+
+ class jsTPS_Transaction {
+
+    /**
+     * This method is called by jsTPS when a transaction is executed.
+     */
+    doTransaction = function()
+    {
+
+    }
+
+    /**
+    * This method is called by jTPS when a transaction is undone.
+    */
+    undoTransaction = function()
+    {
+
+    }
+
+ }
