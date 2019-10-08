5d661fdbb24fed118ee8b558c466545f56a1b10c
Avik Kadakia <avik.kadakia@stonybrook.edu>
2019-10-07 21:33:06 -0400

    Fixed LIST_SCREEN CSS

--- sandbox_hw2/src/components/list_screen/ListItemCard.js
+++ sandbox_hw2/src/components/list_screen/ListItemCard.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 
+var moveUp = require('../../images/Up.png');
+var moveDown = require('../../images/Down.png');
+var deleteCard = require('../../images/Delete.png');
+
 export class ListItemCard extends Component {
     render() {
         return (
@@ -14,7 +18,19 @@ export class ListItemCard extends Component {
                     {this.props.listItem.due_date}
                 </div>
                 <div className='list_item_card_completed'>
-                    {this.props.listItem.completed}
+                    {this.props.listItem.completed === true && <span>Completed</span>}
+                </div>
+                <div className='list_item_card_not_completed'>
+                    {this.props.listItem.completed === false && <span>Pending</span>}
+                </div>
+                <div className='list_item_move_up'>
+                    {<img src={moveUp}></img>}
+                </div>
+                <div className='list_item_move_down'>
+                    {<img src={moveDown}></img>}      
+                </div>
+                <div className='list_item_card_delete'>
+                    {<img src={deleteCard}></img>}
                 </div>
             </div>
         )
--- sandbox_hw2/src/components/list_screen/ListItemsTable.js
+++ sandbox_hw2/src/components/list_screen/ListItemsTable.js
@@ -4,11 +4,12 @@ import ListItemCard from './ListItemCard'
 export class ListItemsTable extends Component {
     render() {
         return (
-            <div id="list_items_container" className="list_item_header_card">
-                <div className="list_item_header_card" />
+            <div id="list_items_container">
+                <div className="list_item_header_card">
                     <div className="list_item_task_header">Task</div>
                     <div className="list_item_due_date_header">Due Date</div>
                     <div className="list_item_status_header">Status</div>
+                </div>
                 
                 {
                     this.props.todoList.items.map((todoItem) => (
@@ -16,7 +17,7 @@ export class ListItemsTable extends Component {
                             key={todoItem.key}
                             listItem={todoItem} />
                     ))
-                    // <ListItemCard id={"list_item_add_card"}>&#10133;</ListItemCard>
+                    // <ListItemCard id={"list_item_add_card"}>&#x2b;</ListItemCard>
                 }
                 
             </div>
